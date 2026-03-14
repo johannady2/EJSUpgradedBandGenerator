1f27449b0ebb1f99808ab7c25fcdcae6039d9a86
johannady2 <johannady2@gmail.com>
2026-03-14 19:27:03 +0900

    erased comments

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-
-
 const getTemplateData = (getOrPost) =>
 {
       let finalHeaderText = headerGenerator(getOrPost, adj,noun);
@@ -22,7 +20,6 @@ const getTemplateData = (getOrPost) =>
 
 const headerGenerator = (getOrPost, adjectiveArray, nounArray) =>
 {
-
 
     let headerText = "Welcome to the Band Name Generator!";
     
@@ -44,9 +41,6 @@ const headerGenerator = (getOrPost, adjectiveArray, nounArray) =>
       return headerText;
 }
 
-
-
-
 app.post("/submit", (req, res) =>
 {
 
@@ -54,32 +48,12 @@ app.post("/submit", (req, res) =>
 
 });
 
-
-
-//Step 3 - Make the styling show up.
-//Hint 1: CSS files are static files!
-//Hint 2: The header and footer are partials.
-//Hint 3: Add the CSS link in header.ejs
-
-//Step 4 - Add a dynamic year to the footer.
-//Hint: Google to find out how to get the current year using JS.
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
 
     res.render("index.ejs", getTemplateData("get"));
 });
-
-//app.post("/submit", (req, res) => {
-  //Step 2 - Make the generate name functionality work
-  //Hint: When the "Generate Name" button in index.ejs is clicked, it should hit up this route.
-  //Then:
-  //1. You should randomly pick an adjective from the const "adj" and a noun from const "noun",
-  //scroll down to see the two arrays.
-  //2. Send the index.ejs as a response and add the adjective and noun to the res.render
-  //3. Test to make sure that the random words display in the h1 element in index.ejs
-//});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
